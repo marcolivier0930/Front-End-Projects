17a40f4364bff1cab96c15adca63190b1435d15b
Marc Blanchard <marcolivier0930@gmail.com>
2019-11-02 14:22:18 -0400

    I have most of the pages done. I set up the page, navigation, and pictures.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//  styling the nav
+$("a").css("color", "white").css("textDecoration", "none");
+
+// styling the header for the about container 
+var aboutHeader = document.querySelector("h1.about-header");
+aboutHeader.style.fontSize = "40px";
+
+// array of quotes
+var arrayQuotes = document.getElementById("array-quotes-styling");
+arrayQuotes.innerHTML = "There was an idea. To bring together a group of remarkable people. To see if we could become something more. So when they needed us, we could fight the battles that they never could.";
